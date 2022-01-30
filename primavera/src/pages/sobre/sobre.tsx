1f2d6f57fe99729a3018f0ce168e3e73c84a9a02
mcsnsx <mcs_nasc@hotmail.com>
2022-01-30 00:43:32 -0300

    Primavera - Sobre Nós

--- primavera/src/pages/sobre/sobre.tsx
+++ primavera/src/pages/sobre/sobre.tsx
@@ -1,7 +1,7 @@
 import React, { } from 'react'
 import { Typography, Box, Grid, } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import './Home.css';
+import './SobreNos.css';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
@@ -14,7 +14,7 @@ function SobreNos() {
                     <Grid container justifyContent="center">
                         <Grid item xs={6}>
                             <Box paddingTop={10}>
-                                <img src="https://i.imgur.com/dBhQmlU.png" alt="logo primavera" className='logoSobre' />
+                                <img src="https://i.imgur.com/DhKsfz2.png?1" alt="logo urbanize" style={{ width: "100vh", height: "56vh" }} />
                             </Box>
                         </Grid>
                     </Grid>
@@ -29,11 +29,8 @@ function SobreNos() {
                                 </Box>
                                 <Box paddingTop={2}>
                                 <Typography className='espaco-letras'>
-                                Com o intuito de promover Saúde da Mulher e conectar pessoas, 
-                                criamos uma rede social onde as pessoas que precisam de ajuda ou 
-                                informação podem entrar e ter contato com profissionais da saúde que 
-                                divulgam informações importantes sobre saúde e bem estar, além de se 
-                                dispor a ajudar a quem precisa.
+                                    Baseada na ODS 11 – Cidades e Comunidades Sustentáveis da ONU,
+                                    Criamos uma rede social com o objetivo de conectar pessoas através de um objetivo comum: dar vida e ocupar espaços públicos urbanos.
                                 </Typography>
                                 </Box>
                             </Box>
@@ -47,12 +44,8 @@ function SobreNos() {
                                 </Box>
                                 <Box paddingTop={2}>
                                 <Typography className='espaco-letras'>
-                                Contando com o apoio de profissionais da saúde e da população, 
-                                o site busca pessoas com intensão de ajudar e pessoas que precisam de ajuda. 
-                                É um espaço onde todos podem entrar para procurar informações ou ter contato 
-                                com profissionais capacitados que divulgam informações sobre saúde e bem estar, 
-                                além de se dispor a ajudar a quem precisa.
-                             </Typography>
+                                    Por meio da divulgação de iniciativas individuais e de coletivos, nossa API irá conectar pessoas e/ou empresas que buscam atuar de forma ativa na promoção de revitalização e ações de resgate da qualidade de vida em parques e praças.
+                                </Typography>
                                 </Box>
                             </Box>
                         </Grid>
@@ -65,11 +58,7 @@ function SobreNos() {
                                 </Box>
                                 <Box paddingBottom={12} paddingTop={2}>
                                 <Typography className='espaco-letras'>
-                                Contando com o apoio de profissionais da saúde e da população, 
-                                o site busca pessoas com intensão de ajudar e pessoas que precisam de ajuda. 
-                                É um espaço onde todos podem entrar para procurar informações ou ter contato 
-                                com profissionais capacitados que divulgam informações sobre saúde e bem estar, 
-                                além de se dispor a ajudar a quem precisa.
+                                    A Promoção de espaços públicos seguros e acessíveis é para todes, principalmente para crianças, mulheres, idosos e pessoas com deficiência.
                                 </Typography>
                                 </Box>
                             </Box>
@@ -81,12 +70,14 @@ function SobreNos() {
                                     Objetivos
                                 </Typography>
                                 </Box>
-                                <Box paddingBottom={12} paddingTop={2}>
+                                <Box paddingTop={2}>
                                 <Typography className='espaco-letras'>
-                                Incentivar a participação ativa da população e de profissionais da 
-                                área da saúde que podem trazer informações e até se dispor a dar suporte 
-                                e tratamento as pessoas que precisam; Identificar possíveis parceiros para 
-                                essa causa; Colaboração das pessoas sem fins lucrativos; 
+                                    Estímulo à participação ativa da sociedade civil em políticas públicas;
+                                    Identificação de agentes locais com maior facilidade na identificação de demandas e resolução de conflitos;
+                                    Colaboração entre setor privado e sociedade civil;
+                                    Controle e fiscalização das ações do poder público;
+                                    Descentralização do poder público;
+                                    Formalização de parcerias resulta em espaços públicos mais qualificados e melhora imagem do setor privado frente à sociedade.
                                 </Typography>
                                 </Box>
                             </Box>
@@ -97,7 +88,7 @@ function SobreNos() {
                     <Grid item xs={6} >
                         <Box paddingBottom={7} >
                             <Typography variant='h3' component='h3' align='center' className='box-texto'>
-                                Quem fez a primavera?
+                                Quem faz a Urbanize?
                             </Typography>
                         </Box>
                     </Grid>
@@ -106,24 +97,24 @@ function SobreNos() {
                 <Grid container xs={12} justifyContent='center' alignItems='center' >
                 <Box justifyContent="center" alignItems="center">
                     <Box  justifyContent="center" alignItems="center" >
-                            <img className='box-fotos' src="https://i.imgur.com/qukKYZm.jpg" alt="Dev Juliana" />
+                            <img className='box-fotos' src="https://i.imgur.com/sYaKCkV.png" alt="Dev Andreia" />
                             </Box>
                             <Box >
                                 <Typography variant='h5' component='h5' align='center'>
-                                    Juliana Macedo
+                                    Andreia Menezes
                                 </Typography>
                                 <Typography align='center'>
                                     Desenvolvedora Full Stack 
                                 </Typography>
                                 <Typography align='center'>
-                                    FUNÇÃO 2
+                                    Estudante de Biomedicina
                                 </Typography>
                             </Box>
                         <Box alignItems="center" justifyContent="center"  className='box-icones' >
-                            <a href=" https://github.com/julianacsmacedo" target="_blank">
+                            <a href=" https://github.com/AndreiaJM" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
                             </a>
-                            <a href=" https://www.linkedin.com/in/juliana-cs-macedo/" target="_blank">
+                            <a href=" https://www.linkedin.com/in/andreiajeniffer/" target="_blank">
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
@@ -131,54 +122,54 @@ function SobreNos() {
                     </Box>
                     <Box>
                     <Box justifyContent="center" alignItems="center">
-                        <img className='box-fotos' src="https://i.imgur.com/clh8TOd.jpg" alt="Dev Maria" />
+                        <img className='box-fotos' src="https://i.imgur.com/pu8PzOP.png" alt="Dev Carlos" />
                     </Box>
 
                     <Box >
                                 <Typography variant='h5' component='h5' align='center'>
-                                    Maria Carolina S. N. Xavier
+                                    Carlos Botelho
                                 </Typography>
                                 <Typography align='center'>
                                     Desenvolvedor Full Stack 
                                 </Typography>
                                 <Typography align='center'>
-                                    Design Gráfico
+                                    Arquiteto Urbanista
                                 </Typography>
                             </Box>
 
                         <Box display="flex" alignItems="center" justifyContent="center" className='box-icones' >
 
-                            <a href="https://github.com/mcsnsx" target="_blank">
+                            <a href="https://github.com/botelhocarlos" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
                             </a>
-                            <a href="https://www.linkedin.com/in/maria-carolina-s-n-xavier-05340b191/" target="_blank">
+                            <a href="https://www.linkedin.com/in/carlos-henrique-botelho/" target="_blank">
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
                     </Box>
                     <Box>
                         <Box>
-                        <img className='box-fotos' src="https://i.imgur.com/qukKYZm.jpg" alt="Dev Talita" />
+                        <img className='box-fotos' src="https://i.imgur.com/dAOvWUg.png" alt="Dev Renan" />
                         </Box>
 
                         <Box >
                                 <Typography variant='h5' component='h5' align='center'>
-                                    Talita Ribeiro
+                                    Renan Moreira
                                 </Typography>
                                 <Typography align='center'>
                                     Desenvolvedor Full Stack 
                                 </Typography>
                                 <Typography align='center'>
-                                    FUNÇÃO 2
+                                    Educador Fisico
                                 </Typography>
                             </Box>
 
                         <Box display="flex" alignItems="center" justifyContent="center" className='box-icones'>
 
-                            <a href=" https://github.com/talitarribeiro" target="_blank">
+                            <a href=" https://github.com/RenanMoreira92" target="_blank">
                                 <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
                             </a>
-                            <a href="https://www.linkedin.com/in/talita-ribeiro-b5a105186/" target="_blank">
+                            <a href="https://www.linkedin.com/in/renan-moreira-518aa6188/" target="_blank">
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
@@ -210,8 +201,37 @@ function SobreNos() {
                                 <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
                             </a>
                         </Box>
-                    </Box>                    
-                         
+                    </Box>
+                    <Box>
+                        <Box>
+                        <img className='box-fotos' src="https://i.imgur.com/E6FvVWJ.png" alt="Dev Valter" />
+                        </Box>
+
+                        <Box >
+                                <Typography variant='h5' component='h5' align='center'>
+                                    Valter Silva
+                                </Typography>
+                                <Typography align='center'>
+                                    Desenvolvedor Full Stack 
+                                </Typography>
+                                <Typography align='center'>
+                                Estudante de Análise e Des. de Sist.
+                                </Typography>
+                            </Box>
+
+                        <Box display="flex" alignItems="center" justifyContent="center" className='box-icones'>
+
+                            <a href="https://github.com/valterchess" target="_blank">
+                                <GitHubIcon style={{ fontSize: 50, color: "#f99e29" }} />
+                            </a>
+                            <a href=" https://www.linkedin.com/in/valter-silva-5a44b1171/" target="_blank">
+                                <LinkedInIcon style={{ fontSize: 60, color: "#f99e29" }} />
+                            </a>
+                        </Box>
+                    </Box>
+                
+
+
                 </Grid>
             </Grid>
         </>
